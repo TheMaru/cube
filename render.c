9ded5b70bbca9f8733d0d9cff507e80402bf1eab
Ben Thiede <mailtothemaru@gmail.com>
2026-02-27 17:24:10 +0100

    implemented Bresenham's line algorithm to clean up lines

--- render.c
+++ render.c
@@ -14,27 +14,73 @@ typedef struct {
   int y;
 } Point2D;
 
-void draw_line(uint32_t* displayBuffer, int width, Point2D p1, Point2D p2) {
-  // calculate step count
-  int dx, dy, steps;
-  dx = abs(p2.x - p1.x);
-  dy = abs(p2.y - p1.y);
-  steps = (dx > dy) ? dx : dy;
+static uint32_t* displayBuffer;
+static int width;
+static int height;
+static uint32_t sideColor = (255 << 24) | (255 << 8);
 
-  uint32_t sideColor = (255 << 24) | (255 << 8);
+static void draw_line_low(int x0, int y0, int x1, int y1) {
+  int dx = abs(x1 - x0);
+  int dy = abs(y1 - y0);
+  int stepY = (y0 < y1) ? 1 : -1; 
+  int error = 2 * dy - dx;
+  int y = y0;
 
-  for (int i = 0; i <= steps; i++) {
-    float t = (float)i / steps;
-    int x = p1.x + (int)(t * (p2.x - p1.x));
-    int y = p1.y + (int)(t * (p2.y - p1.y));
+  for (int x = x0; x <= x1; x++) {
     displayBuffer[y * width + x] = sideColor;
+    error += 2 * dy;
+    if ( error > dx) {
+      y += stepY;
+      error -= 2 * dx;
+    }
   }
 }
 
-void render(uint32_t* displayBuffer, int width, int height, double angle) {
+static void draw_line_high(int x0, int y0, int x1, int y1) {
+  int dx = abs(x1 - x0);
+  int dy = abs(y1 - y0);
+  int stepX = (x0 < x1) ? 1 : -1; 
+  int error = 2 * dx - dy;
+  int x = x0;
+
+  for (int y = y0; y <= y1; y++) {
+    displayBuffer[y * width + x] = sideColor;
+    error += 2 * dx;
+    if ( error > dy) {
+      x += stepX;
+      error -= 2 * dy;
+    }
+  }
+}
+
+static void draw_line(Point2D p1, Point2D p2) {
+  int dx, dy;
+  dx = abs(p2.x - p1.x);
+  dy = abs(p2.y - p1.y);
+  
+  if (dy < dx) {
+    if (p1.x > p2.x) {
+      draw_line_low(p2.x, p2.y, p1.x, p1.y);
+    } else {
+      draw_line_low(p1.x, p1.y, p2.x, p2.y);
+    }
+  } else {
+    if (p1.y > p2.y) {
+      draw_line_high(p2.x, p2.y, p1.x, p1.y);
+    } else {
+      draw_line_high(p1.x, p1.y, p2.x, p2.y);
+    }
+  }
+}
+
+void render(uint32_t* db, int w, int h, double angle) {
   int scale = 200;
-  int centerX = width / 2;
-  int centerY = height / 2;
+  int centerX = w / 2;
+  int centerY = h / 2;
+
+  displayBuffer = db;
+  width = w;
+  height = h;
 
   // define vertices in 3 axis
   Point3D cubeVertices[8] = {
@@ -90,19 +136,19 @@ void render(uint32_t* displayBuffer, int width, int height, double angle) {
     displayBuffer[projectedCubeVertices[i].y * width + projectedCubeVertices[i].x] = pointColor;
   }
 
-  draw_line(displayBuffer, width, projectedCubeVertices[0], projectedCubeVertices[1]);
-  draw_line(displayBuffer, width, projectedCubeVertices[1], projectedCubeVertices[2]);
-  draw_line(displayBuffer, width, projectedCubeVertices[2], projectedCubeVertices[3]);
-  draw_line(displayBuffer, width, projectedCubeVertices[3], projectedCubeVertices[0]);
+  draw_line(projectedCubeVertices[0], projectedCubeVertices[1]);
+  draw_line(projectedCubeVertices[1], projectedCubeVertices[2]);
+  draw_line(projectedCubeVertices[2], projectedCubeVertices[3]);
+  draw_line(projectedCubeVertices[3], projectedCubeVertices[0]);
 
-  draw_line(displayBuffer, width, projectedCubeVertices[4], projectedCubeVertices[5]);
-  draw_line(displayBuffer, width, projectedCubeVertices[5], projectedCubeVertices[6]);
-  draw_line(displayBuffer, width, projectedCubeVertices[6], projectedCubeVertices[7]);
-  draw_line(displayBuffer, width, projectedCubeVertices[7], projectedCubeVertices[4]);
+  draw_line(projectedCubeVertices[4], projectedCubeVertices[5]);
+  draw_line(projectedCubeVertices[5], projectedCubeVertices[6]);
+  draw_line(projectedCubeVertices[6], projectedCubeVertices[7]);
+  draw_line(projectedCubeVertices[7], projectedCubeVertices[4]);
 
-  draw_line(displayBuffer, width, projectedCubeVertices[0], projectedCubeVertices[4]);
-  draw_line(displayBuffer, width, projectedCubeVertices[1], projectedCubeVertices[5]);
-  draw_line(displayBuffer, width, projectedCubeVertices[2], projectedCubeVertices[6]);
-  draw_line(displayBuffer, width, projectedCubeVertices[3], projectedCubeVertices[7]);
+  draw_line(projectedCubeVertices[0], projectedCubeVertices[4]);
+  draw_line(projectedCubeVertices[1], projectedCubeVertices[5]);
+  draw_line(projectedCubeVertices[2], projectedCubeVertices[6]);
+  draw_line(projectedCubeVertices[3], projectedCubeVertices[7]);
 }
 
